632478533b53cd9bbd170548a9424cac0c3ec8b2
Craig Rufener <crufener@outlook.com>
2017-11-01 02:06:39 -0700

    added an about method to the post controller and refactored the about route

--- app/Http/Controllers/PostsController.js
+++ app/Http/Controllers/PostsController.js
@@ -4,6 +4,10 @@ class PostsController {
   * index(req, res) {
   yield res.sendView('home')
   }
+
+  * about(req, res) {
+    yield res.sendView('about')
+    }
 }
 
 module.exports = PostsController
--- app/Http/routes.js
+++ app/Http/routes.js
@@ -3,5 +3,5 @@
 const Route = use('Route')
 
 Route.get('/', 'PostsController.index')
-Route.on('/about').render('about')
+Route.get('/about', 'PostsController.about')
 Route.on('/contact').render('contact')
